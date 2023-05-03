812dc2c25454e0e8ac521168fb83357c83eef23c
ConsolattaChepchirchir <consochep97@gmail.com>
2023-05-03 18:14:06 +0300

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,10 @@ const NavBar = ()=> {
     return(
         
                 <div className=" p-4" id="nav">
-      <h1 className="text-light text-center">EVENT-SEAT-BOOK</h1>
+      <h1 className="text-light text-center">ToTu Event</h1>
       <div className="d-flex justify-content-center mx-5">
         <Link className="btn btn-outline-light text-black" to="/">Home</Link>
-        <Link className="btn btn-outline-light text-black" to="/events">EVENTS</Link>
-        <Link className="btn btn-outline-light text-black" to="/addevents">ADDEVENT</Link>
+        <Link className="btn btn-outline-light text-black" to="/addevents">Login</Link>
       </div>
     </div>
                 
